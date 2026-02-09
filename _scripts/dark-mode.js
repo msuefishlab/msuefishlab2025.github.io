c5756a1dd1cd506fed391226f851850d2f8bc936
jasongallant <jgallant@msu.edu>
2026-02-09 17:36:34 +0000

    Deploying to gh-pages from @ msuefishlab/msuefishlab2025.github.io@4c7087015982062b8136d516570646ce44460620 🚀

--- _scripts/dark-mode.js
+++ _scripts/dark-mode.js
@@ -8,18 +8,18 @@
     window.localStorage.getItem("dark-mode") ?? "false";
 
   const onLoad = () => {
+    const toggle = document.querySelector(".dark-toggle");
     // update toggle button to match loaded mode
-    document.querySelector(".dark-toggle").checked =
-      document.documentElement.dataset.dark === "true";
+    toggle.checked = document.documentElement.dataset.dark === "true";
+
+    // add event listener for dark mode toggle
+    toggle.addEventListener("input", (event) => {
+      const value = event.target.checked;
+      document.documentElement.dataset.dark = value;
+      window.localStorage.setItem("dark-mode", value);
+    });
   };
 
   // after page loads
   window.addEventListener("load", onLoad);
-
-  // when user toggles mode button
-  window.onDarkToggleChange = (event) => {
-    const value = event.target.checked;
-    document.documentElement.dataset.dark = value;
-    window.localStorage.setItem("dark-mode", value);
-  };
 }
